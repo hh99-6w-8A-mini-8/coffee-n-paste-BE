f132d998ce6081a9006e6038f5e218b20cb97373
Kimilm <gnlfla7416@gmail.com>
2022-08-12 17:59:35 +0900

    백엔드 프로젝트 초기 세팅

---
+++ src/main/java/com/mini/coffeenpastebe/CoffeeNPasteBeApplication.java
@@ -0,0 +1,13 @@
+package com.mini.coffeenpastebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeNPasteBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeNPasteBeApplication.class, args);
+    }
+
+}

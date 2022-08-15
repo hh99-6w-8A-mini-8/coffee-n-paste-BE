595bb476c25ea34a5952f9aa7c025ec78f4e36a1
rio <sw_park0205@naver.com>
2022-08-15 19:29:58 +0900

    S3 Bucket 완료

---
+++ src/main/java/com/mini/coffeenpastebe/controller/AwsS3Controller.java
@@ -0,0 +1,31 @@
+package com.mini.coffeenpastebe.controller;
+
+import com.mini.coffeenpastebe.domain.AwsS3;
+import com.mini.coffeenpastebe.service.AwsS3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+public class AwsS3Controller {
+
+    private final AwsS3Service awsS3Service;
+
+    @PostMapping("/api/post/upload-image")
+    public ResponseEntity<?> upload(@RequestPart("file") MultipartFile multipartFile) throws IOException {
+        return ResponseEntity.ok().body(awsS3Service.upload(multipartFile));
+    }
+
+    @DeleteMapping("/api/delete/upload-image")
+    public void remove (AwsS3 awsS3) {
+        awsS3Service.remove(awsS3);
+    }
+
+}
---
+++ src/main/java/com/mini/coffeenpastebe/domain/AwsS3.java
@@ -0,0 +1,13 @@
+package com.mini.coffeenpastebe.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AwsS3 {
+    private String key;
+    private String img;
+}
